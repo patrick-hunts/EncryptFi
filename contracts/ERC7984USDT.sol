5c4ee9453483c8359802d6b54be852246f551f28
patrick-hunts <tvlopz89545@outlook.com>
2025-12-26 15:01:56 +0800

    feat: add cUSDT ERC7984 token

---
+++ contracts/ERC7984USDT.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.27;
+
+import {ERC7984} from "@openzeppelin/confidential-contracts/token/ERC7984/ERC7984.sol";
+import {ZamaEthereumConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
+import {FHE, euint64} from "@fhevm/solidity/lib/FHE.sol";
+
+contract ERC7984USDT is ERC7984, ZamaEthereumConfig {
+    uint64 public constant FAUCET_AMOUNT = 1_000_000_000; // 1000 tokens with 6 decimals
+    uint32 public constant FAUCET_COOLDOWN_SECONDS = 24 hours;
+
+    mapping(address user => uint48 lastClaimedAt) public lastClaimedAt;
+
+    constructor() ERC7984("cUSDT", "cUSDT", "") {}
+
+    function claim() external returns (euint64 minted) {
+        uint48 last = lastClaimedAt[msg.sender];
+        require(last == 0 || block.timestamp - uint256(last) >= FAUCET_COOLDOWN_SECONDS, "Faucet cooldown");
+
+        lastClaimedAt[msg.sender] = uint48(block.timestamp);
+
+        minted = _mint(msg.sender, FHE.asEuint64(FAUCET_AMOUNT));
+    }
+}
